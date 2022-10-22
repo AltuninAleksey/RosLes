aa02c2e18218ce706c056c8669daa5a5bb112091
Ivanov Pavel <87614770+cyberkitaec@users.noreply.github.com>
2022-10-22 13:22:58 +0300

    Добавлены новые модели...
    Добавлены  новые модели, обновлены все сериалайзеры, добавлены новые вьюхи. Появился static с описание стилей админ панели.

---
+++ testDjangosite/djangoForest/models.py
@@ -0,0 +1,197 @@
+from django.db import models
+
+
+class Table(models.Model):
+    name = models.CharField(max_length=300)
+    age = models.IntegerField()
+
+    class Meta:
+        verbose_name = "Таблица"
+        verbose_name_plural = "Таблицы"
+
+
+class Profile(models.Model):
+    FIO = models.CharField(u'ФИО', max_length=255)
+    phoneNumber = models.CharField(u'Номер телефона', max_length=30)
+    email = models.EmailField(u'e-mail адреыс')
+    id_post = models.ForeignKey('Post', on_delete=models.CASCADE, verbose_name='Должность')
+    id_working_breeds = models.ForeignKey('WorkingBreeds', on_delete=models.CASCADE, verbose_name='Рабочая порода')
+    id_role = models.ForeignKey('Role', on_delete=models.CASCADE, verbose_name='Роль')
+    id_branches = models.ForeignKey('Branches', on_delete=models.CASCADE, verbose_name='Филиал')
+
+    class Meta:
+        verbose_name = 'Профили'
+        verbose_name_plural = 'Профили'
+
+
+class List(models.Model):
+    id_sample = models.ForeignKey('Sample', on_delete=models.CASCADE, verbose_name='Перечет')
+    id_breed = models.ForeignKey('Breed', on_delete=models.CASCADE, verbose_name='Порода')
+    id_type_of_reproduction = models.ForeignKey('Reproduction', on_delete=models.CASCADE,
+                                                verbose_name='Вид воспроизводства')
+    to0_2 = models.IntegerField(u'До 0,2')
+    from0_21To0_5 = models.IntegerField(u'0,21 - 0,5')
+    from0_6To1_0 = models.IntegerField(u'0,6 - 1,0')
+    from1_1to1_5 = models.IntegerField(u'1,1 - 1,5')
+    from1_5 = models.IntegerField(u'более 1,5')
+    max_height = models.IntegerField(u'Максимальная высота')
+
+    class Meta:
+        verbose_name = 'Перечет'
+        verbose_name_plural = 'Перечет'
+
+
+class GPS(models.Model):
+    id_sample = models.ForeignKey('Sample', on_delete=models.CASCADE, verbose_name='Проба')
+    latitude = models.FloatField(u'Широта')
+    longitude = models.FloatField(u'Долгота')
+    flag_center = models.IntegerField(u'Флаг центра')
+
+    class Meta:
+        verbose_name = 'GPS'
+        verbose_name_plural = 'GPS'
+
+
+class Region(models.Model):
+    id_subject_rf = models.ForeignKey('SubjectRF', on_delete=models.CASCADE, verbose_name='Субъект РФ')
+    id_forestly = models.ForeignKey('Forestly', on_delete=models.CASCADE, verbose_name='Лесничество')
+    id_district_forestly = models.ForeignKey('DistrictForestly', on_delete=models.CASCADE,
+                                             verbose_name='Участковое лесничество')
+    quarter = models.IntegerField(u'Квартал')
+    soil_lot = models.IntegerField(u'Выдел')
+    sample_region = models.IntegerField(u'Площадь участка')
+
+    class Meta:
+        verbose_name = 'Участок'
+        verbose_name_plural = 'Участок'
+
+
+class ListRegion(models.Model):
+    id_region = models.ForeignKey('Region', on_delete=models.CASCADE, verbose_name='Участок')
+    date = models.DateField(u'Дата')
+    sample_region = models.CharField(u'Плошадь участка', max_length=300)
+    id_subject_RF = models.ForeignKey('SubjectRF', on_delete=models.CASCADE, verbose_name='Субъект РФ')
+    id_forestly = models.ForeignKey('Forestly', on_delete=models.CASCADE, verbose_name='Лесничество')
+    id_district_forestly = models.ForeignKey('DistrictForestly', on_delete=models.CASCADE,
+                                             verbose_name='Участковое лесничество')
+
+    class Meta:
+        verbose_name = 'Перечетная ведомость участка'
+        verbose_name_plural = 'Перечетная ведомость участка'
+
+
+class Sample(models.Model):
+    id_list_region = models.ForeignKey('ListRegion', on_delete=models.CASCADE, verbose_name='Должность')
+    sample_area = models.IntegerField(u'Площадь пробы')
+    id_profile = models.ForeignKey('Profile', on_delete=models.CASCADE, verbose_name='Профиль')
+    id_subject_RF = models.ForeignKey('SubjectRF', on_delete=models.CASCADE, verbose_name='Субъект РФ')
+    id_forestly = models.ForeignKey('Forestly', on_delete=models.CASCADE, verbose_name='Лесничество')
+    id_district_forestly = models.ForeignKey('DistrictForestly', on_delete=models.CASCADE,
+                                             verbose_name='Участковое лесничество')
+    soil_lot = models.IntegerField(u'Выдел')
+    id_list_region = models.ForeignKey('ListRegion', on_delete=models.CASCADE,
+                                       verbose_name='Перечетная ведомость участка')
+
+    class Meta:
+        verbose_name = 'Проба'
+        verbose_name_plural = 'Проба'
+
+
+class Post(models.Model):
+    post_name = models.CharField(max_length=255, verbose_name='Должность')
+
+    def __str__(self):
+        return self.post_name
+
+    class Meta:
+        verbose_name = 'Должности'
+        verbose_name_plural = 'Должности'
+
+
+class WorkingBreeds(models.Model):
+    name_breeds = models.CharField(max_length=255, verbose_name='Наименование рабочей породы')
+
+    def __str__(self):
+        return self.name_breeds
+
+    class Meta:
+        verbose_name = 'Рабочие породы'
+        verbose_name_plural = 'Рабочие породы'
+
+
+class SubjectRF(models.Model):
+    name_subject_RF = models.CharField(max_length=255, verbose_name='Наименования субъекта РФ')
+
+    def __str__(self):
+        return self.name_subject_RF
+
+    class Meta:
+        verbose_name = 'Субъект РФ'
+        verbose_name_plural = 'Субъект РФ'
+
+
+class Role(models.Model):
+    name_role = models.CharField(max_length=300, verbose_name='Наименование')
+
+    def __str__(self):
+        return self.name_role
+
+    class Meta:
+        verbose_name = 'Роли'
+        verbose_name_plural = 'Роли'
+
+
+class Reproduction(models.Model):
+    name_reproduction = models.CharField(max_length=500, verbose_name='Наименование')
+
+    def __str__(self):
+        return self.name_reproduction
+
+    class Meta:
+        verbose_name = 'Вид воспроизводства'
+        verbose_name_plural = 'Вид воспроизводства'
+
+
+class Forestly(models.Model):
+    name_forestly = models.CharField(max_length=500, verbose_name='Название лесничества')
+
+    def __str__(self):
+        return self.name_forestly
+
+    class Meta:
+        verbose_name = 'Лесничество'
+        verbose_name_plural = 'Лесничество'
+
+
+class DistrictForestly(models.Model):
+    name_district_forestly = models.CharField(max_length=500, verbose_name='Наименование участкового лесничества')
+
+    def __str__(self):
+        return self.name_district_forestly
+
+    class Meta:
+        verbose_name = 'Участковое лесничество'
+        verbose_name_plural = 'Участковое лесничество'
+
+
+class Breed(models.Model):
+    name_breed = models.CharField(max_length=350, verbose_name='Наименование породы')
+
+    def __str__(self):
+        return self.name_breed
+
+    class Meta:
+        verbose_name = 'Порода'
+        verbose_name_plural = 'Порода'
+
+
+class Branches(models.Model):
+    name_branch = models.CharField(max_length=350, verbose_name='Наименование филиала')
+    id_subject_RF = models.ForeignKey('SubjectRF', on_delete=models.CASCADE, verbose_name='Субъект РФ')
+
+    def __str__(self):
+        return self.name_branch
+
+    class Meta:
+        verbose_name = 'Филиал'
+        verbose_name_plural = 'Филиал'
---
+++ testDjangosite/djangoForest/serializers.py
@@ -0,0 +1,257 @@
+import io
+
+from rest_framework import serializers
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from .models import *
+
+
+class TableSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Table
+        fields = '__all__'
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = '__all__'
+
+    def create(self, validated_data):
+        return Profile.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.FIO = validated_data.get("FIO", instance.FIO)
+        instance.phoneNumber = validated_data.get("phoneNumber", instance.phoneNumber)
+        instance.email = validated_data.get("email", instance.email)
+        instance.id_post = validated_data.get("id_post", instance.id_post)
+        instance.id_working_breeds = validated_data.get("id_working_breeds", instance.id_working_breeds)
+        instance.id_role = validated_data.get("id_role", instance.id_role)
+        instance.id_branches = validated_data.get("id_branches", instance.id_branches)
+        instance.save()
+        return instance
+
+
+class ListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = List
+        fields = '__all__'
+
+    def create(self, validated_data):
+        return List.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.id_sample = validated_data.get("id_sample", instance.id_sample)
+        instance.id_breed = validated_data.get("id_breed", instance.id_breed)
+        instance.id_type_of_reproduction = validated_data.get("id_type_of_reproduction", instance.id_type_of_reproduction)
+        instance.to0_2 = validated_data.get("to0_2", instance.to0_2)
+        instance.from0_21To0_5 = validated_data.get("from0_21To0_5", instance.from0_21To0_5)
+        instance.from0_6To1_0 = validated_data.get("from0_6To1_0", instance.from0_6To1_0)
+        instance.from1_1to1_5 = validated_data.get("from1_1to1_5", instance.from1_1to1_5)
+        instance.from1_5 = validated_data.get("from1_5", instance.from1_5)
+        instance.max_height = validated_data.get("max_height", instance.max_height)
+        instance.save()
+        return instance
+
+
+class GPSSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GPS
+        fields = '__all__'
+
+    def create(self, validated_data):
+        return GPS.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.id_sample = validated_data.get("id_sample", instance.id_sample)
+        instance.latitude = validated_data.get("latitude", instance.latitude)
+        instance.longitude = validated_data.get("longitude", instance.longitude)
+        instance.flag_center = validated_data.get("flag_center", instance.flag_center)
+        instance.save()
+        return instance
+
+
+class RegionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Region
+        fields = '__all__'
+
+    def create(self, validated_data):
+        return Region.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.id_subject_rf = validated_data.get("id_subject_rf", instance.id_subject_rf)
+        instance.id_forestly = validated_data.get("id_forestly", instance.id_forestly)
+        instance.id_district_forestly = validated_data.get("id_district_forestly", instance.id_district_forestly)
+        instance.quarter = validated_data.get("quarter", instance.quarter)
+        instance.soil_lot = validated_data.get("soil_lot", instance.soil_lot)
+        instance.sample_region = validated_data.get("sample_region", instance.sample_region)
+        instance.save()
+        return instance
+
+
+class ListRegionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ListRegion
+        fields = '__all__'
+
+    def create(self, validated_data):
+        return ListRegion.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.id_region = validated_data.get("id_region", instance.id_region)
+        instance.date = validated_data.get("date", instance.date)
+        instance.sample_region = validated_data.get("sample_region", instance.sample_region)
+        instance.id_subject_RF = validated_data.get("id_subject_RF", instance.id_subject_RF)
+        instance.id_forestly = validated_data.get("id_forestly", instance.id_forestly)
+        instance.id_district_forestly = validated_data.get("id_district_forestly", instance.id_district_forestly)
+        instance.save()
+        return instance
+
+
+class SampleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Sample
+        fields = '__all__'
+
+    def create(self, validated_data):
+        return Sample.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.id_list_region = validated_data.get("id_list_region", instance.id_list_region)
+        instance.sample_area = validated_data.get("sample_area", instance.sample_area)
+        instance.id_profile = validated_data.get("id_profile", instance.id_profile)
+        instance.id_subject_RF = validated_data.get("id_subject_RF", instance.id_subject_RF)
+        instance.id_forestly = validated_data.get("id_forestly", instance.id_forestly)
+        instance.id_district_forestly = validated_data.get("id_district_forestly", instance.id_district_forestly)
+        instance.soil_lot = validated_data.get("soil_lot", instance.soil_lot)
+        instance.id_list_region = validated_data.get("id_list_region", instance.id_list_region)
+        instance.save()
+        return instance
+
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+    def create(self, validated_data):
+        return Post.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.post_name = validated_data.get("post_name", instance.post_name)
+        instance.save()
+        return instance
+
+
+class WorkingBreedsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WorkingBreeds
+        fields = '__all__'
+
+    def create(self, validated_data):
+        return WorkingBreeds.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name_breeds = validated_data.get("name_breeds", instance.name_breeds)
+        instance.save()
+        return instance
+
+
+class SubjectRFSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SubjectRF
+        fields = '__all__'
+
+    def create(self, validated_data):
+        return SubjectRF.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name_subject_RF = validated_data.get("name_subject_RF", instance.name_subject_RF)
+        instance.save()
+        return instance
+
+
+class RoleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Role
+        fields = '__all__'
+
+    def create(self, validated_data):
+        return Role.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name_role = validated_data.get("name_role", instance.name_role)
+        instance.save()
+        return instance
+
+
+class ReproductionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Reproduction
+        fields = '__all__'
+
+    def create(self, validated_data):
+        return Reproduction.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name_reproduction = validated_data.get("name_reproduction", instance.name_reproduction)
+        instance.save()
+        return instance
+
+
+class ForestlySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Forestly
+        fields = '__all__'
+
+    def create(self, validated_data):
+        return Forestly.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name_forestly = validated_data.get("name_forestly", instance.name_forestly)
+        instance.save()
+        return instance
+
+
+class DistrictForestlySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DistrictForestly
+        fields = '__all__'
+
+    def create(self, validated_data):
+        return DistrictForestly.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name_district_forestly = validated_data.get("name_district_forestly", instance.name_district_forestly)
+        instance.save()
+        return instance
+
+
+class BreedSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Breed
+        fields = '__all__'
+
+    def create(self, validated_data):
+        return Breed.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name_breed = validated_data.get("name_breed", instance.name_breed)
+        instance.save()
+        return instance
+
+
+class BranchesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Branches
+        fields = '__all__'
+
+    def create(self, validated_data):
+        return Branches.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.name_branch = validated_data.get("name_branch", instance.name_branch)
+        instance.id_subject_RF = validated_data.get("id_subject_RF", instance.id_subject_RF)
+        instance.save()
+        return instance
