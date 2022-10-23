ddfa1d02277b4dbf971264654e8eed218a1c3be7
Ivanov Pavel <87614770+cyberkitaec@users.noreply.github.com>
2022-10-23 18:53:21 +0300

    Добавлен verbose_name для конфига

--- testDjangosite/djangoForest/apps.py
+++ testDjangosite/djangoForest/apps.py
@@ -4,6 +4,4 @@ from django.apps import AppConfig
 class TestapiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'djangoForest'
-
-    class Meta:
-        verbose_name = "Взаимодействие с БД"
+    verbose_name = "Администрирование системы хранения данных"
