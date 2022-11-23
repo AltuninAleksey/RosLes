03979f61d7cd50463cfd345d0a112151f29dc88a
Ivanov Pavel <87614770+cyberkitaec@users.noreply.github.com>
2022-11-24 00:05:47 +0300

    Update settings.py

--- testDjangosite/testDjangosite/settings.py
+++ testDjangosite/testDjangosite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-e3jq*c1(yxlmcs*&89)m_+tc*x)dykr^=mcbmq=(qznwackn#f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
