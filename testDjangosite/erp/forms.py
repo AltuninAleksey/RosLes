c376b67bb03befb64bac1cee9d118cff174d6932
Ivanov Pavel <87614770+cyberkitaec@users.noreply.github.com>
2022-11-27 23:33:38 +0300

    Add files via upload

--- testDjangosite/erp/forms.py
+++ testDjangosite/erp/forms.py
@@ -38,13 +38,95 @@ class SubjectRFUpdateForm(forms.ModelForm):
             'name_subject_RF': forms.TextInput(),
         }
 
-
 class ForestlyForm(forms.ModelForm):
     class Meta:
         model = Forestly
         fields = ['name_forestly', 'id_subject_rf']
         # queryset_rf = SubjectRF.objects.all()
-        id_subject_rf = forms.ModelChoiceField(queryset=SubjectRF.objects.all(), empty_label=None, to_field_name='subjectrf'),
+        id_subject_rf = forms.ModelChoiceField(queryset=SubjectRF.objects.all(), empty_label=None,
+                                               to_field_name='subjectrf')
         widgets = {
             'name_forestly': forms.TextInput(attrs={'placeholder': 'Лесничество'}),
+        }
+
+
+class ForestlyUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Forestly
+        fields = ['name_forestly', 'id_subject_rf']
+        id_subject_rf = forms.ModelChoiceField(queryset=SubjectRF.objects.all(), empty_label=None,
+                                               to_field_name='subjectrf')
+        widgets = {
+            'name_forestly': forms.TextInput()
+        }
+
+
+class DistrictForestlyForm(forms.ModelForm):
+    class Meta:
+        model = DistrictForestly
+        fields = ['name_district_forestly', 'id_forestly']
+        id_forestly = forms.ModelChoiceField(queryset=Forestly.objects.all(), empty_label=None,
+                                             to_field_name='forestly')
+        widgets = {
+            'name_district_forestly': forms.TextInput(attrs={'placeholder': 'Наименование лесничества'},)
+        }
+
+
+class DistrictForestlyUpdateForm(forms.ModelForm):
+    class Meta:
+        model = DistrictForestly
+        fields = ['name_district_forestly', 'id_forestly']
+        id_forestly = forms.ModelChoiceField(queryset=Forestly.objects.all(), empty_label=None,
+                                             to_field_name='forestly')
+        widgets = {
+            'name_district_forestly': forms.TextInput()
+        }
+
+
+class RoleForm(forms.ModelForm):
+    class Meta:
+        model = Role
+        fields = ['name_role']
+        widgets = {
+            'name_role': forms.TextInput(attrs={'placeholder': 'Наименование'})
+        }
+
+
+class RoleUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Role
+        fields = ['name_role']
+        widgets = {
+            'name_role': forms.TextInput()
+        }
+
+
+class BranchForm(forms.ModelForm):
+    class Meta:
+        model = Branches
+        fields = ['name_branch']
+        widgets = {
+            'name_branch': forms.TextInput(attrs={'placeholder': 'Наименование филиала'}),
+        }
+
+
+class BranchUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Branches
+        fields = ['name_branch']
+        widgets = {
+            'name_branch': forms.TextInput(),
+        }
+
+
+class GPSForm(forms.ModelForm):
+    class Meta:
+        model = GPS
+        fields = ['latitude', 'longitude', 'flag_center']
+        id_sample = forms.ModelChoiceField(queryset=Sample.objects.all(), empty_label=None,
+                                           to_field_name='sample')
+        widgets = {
+            'latitude': forms.TextInput(attrs={'placeholder': 'Широта'}),
+            'longitude': forms.TextInput(attrs={'placeholder': 'Долгота'}),
+            'flag_center': forms.TextInput(attrs={'placeholder': 'Флаг'}),
         }
