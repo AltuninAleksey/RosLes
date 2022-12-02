a99a1e2ba7e712a014a2ecac81ad802b816aee46
Ivanov Pavel <87614770+cyberkitaec@users.noreply.github.com>
2022-12-02 15:22:28 +0300

    new app

---
+++ testDjangosite/general_docs/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from general_docs.views import *
+
+
+urlpatterns = [
+    path('general_docs', index),
+    path('PurposeOfForests', PurposeOfForestsView.as_view(), name='purpose'),
+    path('ForestProtectionCategory', ForestProtectionCategoryView.as_view()),
+    path('CategoryOfForestFundLands', CategoryOfForestFundLandsView.as_view()),
+    path('MethodOfReforestations', MethodOfReforestationView.as_view()),
+    path('BonitetOrlov', BonitetOrlovView.as_view()),
+    path('TypeForestGrowingConditions', TypeForestGrowingConditionsView.as_view()),
+    path('economy', EconomyView.as_view()),
+    path('AccordanceMolodKrAndTPPL', AccordanceMolodKrAndTPPLView.as_view()),
+    path('AccordanceNoneAccordanceEconomy', AccordanceNoneAccordanceEconomyView.as_view()),
+    path('CategoryGroundLFInNoneAccordance', CategoryGroundLFInNoneAccordanceView.as_view()),
+
+]
+
---
+++ testDjangosite/general_docs/views.py
@@ -0,0 +1,131 @@
+from django.shortcuts import render, redirect
+from djangoForest.models import *
+from django.views.generic import ListView, DetailView
+
+menu = [
+    {'title': 'Целевое назначение лесов', 'url_name': 'PurposeOfForests'},
+    {'title': 'Категория защитности лесов', 'url_name': 'ForestProtectionCategory'},
+    {'title': 'Категория земель лесного фонда', 'url_name': 'CategoryOfForestFundLands'},
+    {'title': 'Способ лесовосстановления', 'url_name': 'MethodOfReforestations'},
+    {'title': 'Бонитет по Орлову', 'url_name': 'BonitetOrlov'},
+    {'title': 'Тип лесорастительный условий', 'url_name': 'TypeForestGrowingConditions'},
+    {'title': 'Хозяйство', 'url_name': 'economy'},
+    {'title': 'Соответ. молодняка кр. и тр. ПЛ', 'url_name': 'AccordanceMolodKrAndTPPL'},
+    {'title': 'Соответ. не соответ. хозяйству', 'url_name': 'AccordanceNoneAccordanceEconomy'},
+    {'title': 'Кат. земель лф в случ. несоотв.', 'url_name': 'CategoryGroundLFInNoneAccordance'},
+
+]
+
+def index(request):
+    return redirect('purpose')
+
+
+class PurposeOfForestsView(DetailView):
+    model = PurposeOfForests
+    template_name = 'general_docs/html/PurposeOfForests.html'
+    queryset = PurposeOfForests.objects.all()
+
+    def get_queryset(self):
+        return PurposeOfForests.objects.all()
+
+    def get(self, request):
+        return render(request, self.template_name, {'menu': menu, 'queryset': self.queryset})
+
+
+class ForestProtectionCategoryView(ListView):
+    template_name = 'general_docs/html/ForestProtectionCategory.html'
+    queryset = ForestProtectionCategory.objects.all()
+
+    def get_queryset(self):
+        return ForestProtectionCategory.objects.all()
+
+    def get(self, request):
+        return render(request, self.template_name, {'menu': menu, 'queryset': self.queryset})
+
+
+class CategoryOfForestFundLandsView(ListView):
+    template_name = 'general_docs/html/CategoryOfForestFundLands.html'
+    queryset = CategoryOfForestFundLands.objects.all()
+
+    def get_queryset(self):
+        return CategoryOfForestFundLands.objects.all()
+
+    def get(self, request):
+        return render(request, self.template_name, {'menu': menu, 'queryset': self.queryset})
+
+
+class MethodOfReforestationView(ListView):
+    template_name = 'general_docs/html/MethodOfReforestations.html'
+    queryset = MethodOfReforestation.objects.all()
+
+    def get_queryset(self):
+        return MethodOfReforestation.objects.all()
+
+    def get(self, request):
+        return render(request, self.template_name, {'menu': menu, 'queryset': self.queryset})
+
+
+class BonitetOrlovView(ListView):
+    template_name = 'general_docs/html/BonitetOrlov.html'
+    queryset = BonitetOrlov.objects.all()
+
+    def get_queryset(self):
+        return BonitetOrlov.objects.all()
+
+    def get(self, request):
+        return render(request, self.template_name, {'menu': menu, 'queryset': self.queryset})
+
+
+class TypeForestGrowingConditionsView(ListView):
+    template_name = 'general_docs/html/TypeForestGrowingConditions.html'
+    queryset = TypeForestGrowingConditions.objects.all()
+
+    def get_queryset(self):
+        return TypeForestGrowingConditions.objects.all()
+
+    def get(self, request):
+        return render(request, self.template_name, {'menu': menu, 'queryset': self.queryset})
+
+
+class EconomyView(ListView):
+    template_name = 'general_docs/html/economy.html'
+    queryset = Economy.objects.all()
+
+    def get_queryset(self):
+        return Economy.objects.all()
+
+    def get(self, request):
+        return render(request, self.template_name, {'menu': menu, 'queryset': self.get_queryset()})
+
+
+class AccordanceMolodKrAndTPPLView(ListView):
+    template_name = 'general_docs/html/AccordanceMolodKrAndTPPL.html'
+    queryset = AccordanceMolodKrAndTPPL.objects.all()
+
+    def get_queryset(self):
+        return AccordanceMolodKrAndTPPL.objects.all()
+
+    def get(self, request):
+        return render(request, self.template_name, {'menu': menu, 'queryset': self.queryset})
+
+
+class AccordanceNoneAccordanceEconomyView(ListView):
+    template_name = 'general_docs/html/AccordanceNoneAccordanceEconomy.html'
+    queryset = AccordanceNoneAccordanceEconomy.objects.all()
+
+    def get_queryset(self):
+        return AccordanceNoneAccordanceEconomy.objects.all()
+
+    def get(self, request):
+        return render(request, self.template_name, {'menu': menu, 'queryset': self.queryset})
+
+
+class CategoryGroundLFInNoneAccordanceView(ListView):
+    template_name = 'general_docs/html/CategoryGroundLFInNoneAccordance.html'
+    queryset = CategoryGroundLFInNoneAccordance.objects.all()
+
+    def get_queryset(self):
+        return CategoryGroundLFInNoneAccordance.objects.all()
+
+    def get(self, request):
+        return render(request, self.template_name, {'menu': menu, 'queryset': self.queryset})
