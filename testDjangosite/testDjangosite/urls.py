0becac886fa195b2abde1978409fe3f494e58c6f
cyberkitaec <addkudi@mail.ru>
2022-12-11 21:44:19 +0300

    .

--- testDjangosite/testDjangosite/urls.py
+++ testDjangosite/testDjangosite/urls.py
@@ -21,8 +21,8 @@ from djangoForest.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('djangoForest.urls')),
-    path('api/post/sample', CreateSampleAndOther.as_view()),
-    path('api/post/sample/<int:pk>', CreateSampleAndOther.as_view()),
+    path('sampleandother', CreateSampleAndOther.as_view()),
+    path('sampleandother/<int:pk>', CreateSampleAndOther.as_view()),
     path('api/v1/gettable', TestAPIView.as_view()),
     path('allforest', AllForestlyViewSet.as_view({'get': 'list'})),
     path('getlistdata/<int:pk>', GetDocumentListData.as_view({'get': 'list'})),
