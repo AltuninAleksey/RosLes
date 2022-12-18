38e6f451e9278c94acff7622d2314b285f8142f3
Ivanov Pavel <87614770+cyberkitaec@users.noreply.github.com>
2022-12-18 17:13:35 +0300

    timezone

--- testDjangosite/testDjangosite/settings.py
+++ testDjangosite/testDjangosite/settings.py
@@ -82,16 +82,17 @@ WSGI_APPLICATION = 'testDjangosite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
+        'TIME_ZONE': 'UTC',
         'NAME': 'rosLes',
         'USER': 'PythonUser',
         'PASSWORD': 'PPythonUserassword',
