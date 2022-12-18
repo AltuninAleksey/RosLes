77ae889dccf44ed1a80ab38673ebe02c77857e9e
cyberkitaec <addkudi@mail.ru>
2022-12-18 16:51:58 +0300

    settings

--- testDjangosite/testDjangosite/settings.py
+++ testDjangosite/testDjangosite/settings.py
@@ -82,12 +82,25 @@ WSGI_APPLICATION = 'testDjangosite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'rosLes',
+        'USER': 'PythonUser',
+        'PASSWORD': 'PPythonUserassword',
+        'HOST': '90.156.208.88',
+        'PORT': '3306'
+
     }
 }
+
 
 
 # Password validation
