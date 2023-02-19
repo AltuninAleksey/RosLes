ff39d33bb2ae0d984059db0af0dfce14d2bfb298
cyberkitaec <addkudi@mail.ru>
2023-02-19 19:52:47 +0300

    photopoint request update

--- testDjangosite/djangoForest/views.py
+++ testDjangosite/djangoForest/views.py
@@ -626,7 +626,8 @@ class PhotoPointView(APIView):
             return HttpResponse({"no image"}, status=400)
         serializer_photo = PhotoPointSerializer(data=request.data)
         if serializer_photo.is_valid():
-            serializer_photo.save(photo=request.data.get('photo'))
+            serializer_photo.save(id_sample_id = request.data.get('id'),
+            photo=request.data.get('photo'))
             return HttpResponse(status=201)
         return Response(serializer_photo.errors, status=400)
 
