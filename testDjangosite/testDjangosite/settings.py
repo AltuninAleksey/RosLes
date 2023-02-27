a8afcce759364b4552c520ae5507d6c5912a73f0
Ivanov Pavel <87614770+cyberkitaec@users.noreply.github.com>
2023-02-27 17:53:25 +0300

    add mimetypes

--- testDjangosite/testDjangosite/settings.py
+++ testDjangosite/testDjangosite/settings.py
@@ -9,11 +9,13 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import mimetypes
 import rest_framework
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+mimetypes.add_type("text/css", ".css", True)
+mimetypes.add_type("application/json", ".js", True)
 print(BASE_DIR)
 
 
