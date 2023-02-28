ea27cf1c5e64f62f79c1dd2998cdf686b1b81700
cyberkitaec <addkudi@mail.ru>
2023-02-28 15:49:20 +0300

    new sett debug false

--- testDjangosite/testDjangosite/settings.py
+++ testDjangosite/testDjangosite/settings.py
@@ -15,7 +15,7 @@ import rest_framework
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 mimetypes.add_type("text/css", ".css", True)
-mimetypes.add_type("application/json", ".js", True)
+# mimetypes.add_type("application/json", ".js", True)
 mimetypes.add_type("image/png", ".png", True)
 mimetypes.add_type("image/jpg", ".jpg", True)
 print(BASE_DIR)
--- testDjangosite/testDjangosite/urls.py
+++ testDjangosite/testDjangosite/urls.py
@@ -14,11 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from djangoForest.views import *
+from django.views.static import serve
+from testDjangosite import settings
 
 
 urlpatterns = [
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     path('admin/', admin.site.urls),
     path('', include('djangoForest.urls')),
     path('upload', PhotoPointView.as_view()),
