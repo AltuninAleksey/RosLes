380c6275b56ac4aa8f3de50082baa7eb4fafbed5
Shock2559 <polenok.maksim.2001@mail.ru>
2023-03-23 21:40:04 +0300

    Обновление front

--- testDjangosite/erp/static/js/common/common_business.js
+++ testDjangosite/erp/static/js/common/common_business.js
@@ -9,3 +9,48 @@ CommonBusiness.getAllForest = async function() {
 
     return requestData.data;
 }
+
+CommonBusiness.getAllSubjectrf = async function() {
+
+    var requestData = await axios({
+        method: 'get',
+        url: urlGlobal + "/subjectRF",
+        responseType: 'json'
+    });
+
+    return requestData.data.get;
+}
+
+CommonBusiness.getForestlyByIdSubjectrf = async function(id) {
+
+    var requestData = await axios({
+        method: 'get',
+        url: urlGlobal + "/getforestlybysubjectid/" + id,
+        responseType: 'json'
+    });
+
+    return requestData.data.data;
+}
+
+CommonBusiness.getDistrictForestlyByIdForestly = async function(id) {
+
+    var requestData = await axios({
+        method: 'get',
+        url: urlGlobal + "/getdistrictbyforestly/" + id,
+        responseType: 'json'
+    });
+
+    return requestData.data.data;
+
+}
+
+CommonBusiness.getQuarterByIdDistrictForestly = async function(id) {
+
+    var requestData = await axios({
+        method: 'get',
+        url: urlGlobal + "/getquarterbydistrictid/" + id,
+        responseType: 'json'
+    });
+
+    return requestData.data.data;
+}
--- testDjangosite/staticfiles/js/statementRecalculations/statementRecalculations_construct_view.js
+++ testDjangosite/staticfiles/js/statementRecalculations/statementRecalculations_construct_view.js
@@ -10,6 +10,12 @@ async function buildStatementRecalculationsTbody() {
     APP.district_forestly = allForestData.district_forestly;
     APP.quarter = allForestData.quarter;
 
+    updateDataInStatementRecalculationsTbody(data);
+
+    setEventForElementsFilter();
+}
+
+function updateDataInStatementRecalculationsTbody(data) {
     var tableBody = document.getElementById("statementRecalculations_tbody");
     var newHtml = "";
 
@@ -31,19 +37,251 @@ async function buildStatementRecalculationsTbody() {
                         </tr> \n`;
     }
     tableBody.innerHTML = newHtml;
-
 }
 
-/*
-{
-    bSubjectrf: true,
-    idSubjectrf: 1,
-    bForestly: false,
-    idForestly: 0,
-    bDistrictForestly: true,
-    idDistrictForestly: 2,
-    bQuarter: false,
-    idQuarter: 0,
-    bDate: false,
-    date: ""
-}*/
+async function setEventForElementsFilter() {
+
+    await setOptionInSubject();
+
+    document
+        .querySelector("#checkbox_filter_subject_rf")
+        .addEventListener('click', async (e)=>{
+            let subjectNode = document.querySelector("#filter_subject_rf");
+            if(e.target.checked) {
+                subjectNode.removeAttribute("disabled")
+                subjectNode.addEventListener('change', async (e)=>{
+                    await setOptionInForestly(StatementRecalculationsBusiness.TypeData.BYID);
+                });
+                await setOptionInForestly(StatementRecalculationsBusiness.TypeData.BYID);
+            } else {
+                subjectNode.setAttribute("disabled", "on");
+                await setOptionInForestly(StatementRecalculationsBusiness.TypeData.ALL);
+            }
+        });
+
+    await setOptionInForestly(StatementRecalculationsBusiness.TypeData.ALL);
+
+    document
+        .querySelector("#checkbox_filter_forestly")
+        .addEventListener('click', async (e)=>{
+            let forestlyNode = document.querySelector("#filter_forestly");
+            if(e.target.checked) {
+                forestlyNode.removeAttribute("disabled");
+                forestlyNode.addEventListener('change', async (e)=>{
+                    await setOptionInDistrictForestly(StatementRecalculationsBusiness.TypeData.BYID);
+                });
+                await setOptionInDistrictForestly(StatementRecalculationsBusiness.TypeData.BYID);
+            } else {
+                forestlyNode.setAttribute("disabled", "on");
+                await setOptionInDistrictForestly(StatementRecalculationsBusiness.TypeData.ALL);
+            }
+        });
+
+    await setOptionInDistrictForestly(StatementRecalculationsBusiness.TypeData.ALL);
+
+    document
+        .querySelector("#checkbox_filter_district_forestly")
+        .addEventListener('click', async (e)=>{
+            let districtForestly = document.querySelector("#filter_district_forestly");
+            if(e.target.checked) {
+                districtForestly.removeAttribute("disabled");
+                districtForestly.addEventListener('change', async (e)=>{
+                    await setOptionInQuarter(StatementRecalculationsBusiness.TypeData.BYID);
+                });
+                await setOptionInQuarter(StatementRecalculationsBusiness.TypeData.BYID);
+            } else {
+                districtForestly.setAttribute("disabled", "on");
+                await setOptionInQuarter(StatementRecalculationsBusiness.TypeData.ALL);
+            }
+        });
+
+    await setOptionInQuarter(StatementRecalculationsBusiness.TypeData.ALL);
+
+    document
+        .querySelector("#checkbox_filter_quartal")
+        .addEventListener('click', (e)=>{
+            if(e.target.checked) {
+                document
+                    .querySelector("#filter_quartal")
+                    .removeAttribute("disabled");
+            } else {
+                document
+                    .querySelector("#filter_quartal")
+                    .setAttribute("disabled", "on");
+            }
+        });
+
+    document
+        .querySelector("#checkbox_filter_date_start")
+        .addEventListener('click', (e)=>{
+            if(e.target.checked) {
+                document
+                    .querySelector("#filter_date_start")
+                    .removeAttribute("readonly");
+            } else {
+                document
+                    .querySelector("#filter_date_start")
+                    .setAttribute("readonly", "on");
+            }
+        });
+
+    document
+        .querySelector("#checkbox_filter_date_end")
+        .addEventListener('click', (e)=>{
+            if(e.target.checked) {
+                document
+                    .querySelector("#filter_date_end")
+                    .removeAttribute("readonly");
+            } else {
+                document
+                    .querySelector("#filter_date_end")
+                    .setAttribute("readonly", "on");
+            }
+        });
+
+    document
+        .querySelector("#but_filtr")
+        .addEventListener('click', async (e)=>{
+            await searchByFilter();
+        });
+}
+
+async function setOptionInSubject() {
+    let subjects = await CommonBusiness.getAllSubjectrf();
+    let subjectNode = document.querySelector("#filter_subject_rf");
+
+    let newHtml = "";
+
+    for(var i = 0; i < subjects.length; i++) {
+        if(i == 0) {
+            newHtml = newHtml + "<option selected value=\"" + subjects[i].id + "\">" + subjects[i].name_subject_RF + "</option>";
+        } else {
+            newHtml = newHtml + "<option value=\"" + subjects[i].id + "\">" + subjects[i].name_subject_RF + "</option>";
+        }
+    }
+
+    subjectNode.innerHTML = newHtml;
+
+    await setOptionInForestly(StatementRecalculationsBusiness.TypeData.BYID);
+}
+
+async function setOptionInForestly(status) {
+    let forestlyNode = document.querySelector("#filter_forestly");
+    let forestly;
+
+    if(status == StatementRecalculationsBusiness.TypeData.BYID) {
+        let subjectNode = document.querySelector("#filter_subject_rf");
+        forestly = await CommonBusiness.getForestlyByIdSubjectrf(subjectNode.value);
+    } else {
+        forestly = APP.forestly;
+    }
+
+    var newHtml = "";
+
+    for(var i = 0; i < forestly.length; i++) {
+        if(i == 0) {
+            newHtml = newHtml + "<option selected value=\"" + forestly[i].id + "\">" + forestly[i].name_forestly + "</option>";
+        } else {
+            newHtml = newHtml + "<option value=\"" + forestly[i].id + "\">" + forestly[i].name_forestly + "</option>";
+        }
+    }
+    forestlyNode.innerHTML = newHtml;
+
+    await setOptionInDistrictForestly(StatementRecalculationsBusiness.TypeData.BYID);
+}
+
+async function setOptionInDistrictForestly(status) {
+    let districtForestlyNode = document.querySelector("#filter_district_forestly");
+    let districtForestly;
+
+    if(status == StatementRecalculationsBusiness.TypeData.BYID) {
+        let forestlyNode = document.querySelector("#filter_forestly");
+        districtForestly = await CommonBusiness.getDistrictForestlyByIdForestly(forestlyNode.value);
+    } else {
+        districtForestly = APP.district_forestly;
+    }
+
+    var newHtml = "";
+
+    for(var i = 0; i < districtForestly.length; i++) {
+        if(i == 0) {
+            newHtml = newHtml + "<option selected value=\"" + districtForestly[i].id + "\">" + districtForestly[i].name_district_forestly + "</option>";
+        } else {
+            newHtml = newHtml + "<option value=\"" + districtForestly[i].id + "\">" + districtForestly[i].name_district_forestly + "</option>";
+        }
+    }
+
+    districtForestlyNode.innerHTML = newHtml;
+
+    await setOptionInQuarter(StatementRecalculationsBusiness.TypeData.BYID);
+}
+
+async function setOptionInQuarter(status) {
+
+    let quarterNode = document.querySelector("#filter_quartal");
+    let quarter;
+
+    if(status == StatementRecalculationsBusiness.TypeData.BYID) {
+        let districtForestlyNode = document.querySelector("#filter_district_forestly");
+        quarter = await CommonBusiness.getQuarterByIdDistrictForestly(districtForestlyNode.value);
+    } else {
+        quarter = APP.quarter;
+    }
+
+    var newHtml = "";
+    for(var j = 0; j < quarter.length; j++) {
+        if(j == 0) {
+            newHtml = newHtml + "<option selected value=\"" + quarter[j].id + "\">" + quarter[j].quarter_name + "</option>";
+        } else {
+            newHtml = newHtml + "<option value=\"" + quarter[j].id + "\">" + quarter[j].quarter_name + "</option>";
+        }
+    }
+    quarterNode.innerHTML = newHtml;
+}
+
+async function searchByFilter() {
+
+    let checkboxFilterSubjectRFNode = document.querySelector("#checkbox_filter_subject_rf");
+    let subjectRFNode = document.querySelector("#filter_subject_rf");
+    let checkboxFilterForestlyNode = document.querySelector("#checkbox_filter_forestly");
+    let forestlyNode = document.querySelector("#filter_forestly");
+    let checkboxFilterDistrictForestlyNode = document.querySelector("#checkbox_filter_district_forestly");
+    let districtForestlyNode = document.querySelector("#filter_district_forestly");
+    let checkboxFilterQuartalNode = document.querySelector("#checkbox_filter_quartal");
+    let quartalNode = document.querySelector("#filter_quartal")
+    let checkboxFilterDateStartNode = document.querySelector("#checkbox_filter_date_start");
+    let dateStartNode = document.querySelector("#filter_date_start");
+    let checkboxFilterDateEnd = document.querySelector("#checkbox_filter_date_end");
+    let dateEndNode = document.querySelector("#filter_date_end");
+
+    let data;
+
+    if(checkboxFilterSubjectRFNode.checked || checkboxFilterForestlyNode.checked
+    || checkboxFilterDistrictForestlyNode.checked || checkboxFilterQuartalNode.checked
+    || checkboxFilterDateStartNode.checked || checkboxFilterDateEnd.checked) {
+
+        let responseData = {
+            bSubjectrf: checkboxFilterSubjectRFNode.checked,
+            idSubjectrf: subjectRFNode.value,
+            bForestly: checkboxFilterForestlyNode.checked,
+            idForestly: forestlyNode.value,
+            bDistrictForestly: checkboxFilterDistrictForestlyNode.checked,
+            idDistrictForestly: districtForestlyNode.value,
+            bQuarter: checkboxFilterQuartalNode.checked,
+            idQuarter: quartalNode.value,
+            bDate: checkboxFilterDateStartNode.checked,
+            date: dateStartNode.value,
+            bDateSec: checkboxFilterDateEnd.checked,
+            bDate: dateEndNode.value
+        };
+
+
+        data = await StatementRecalculationsBusiness.getCreateRecalculationDetailData(responseData);
+    } else {
+
+        data = await StatementRecalculationsBusiness.getAllStatementList();
+    }
+
+
+    updateDataInStatementRecalculationsTbody(data);
+}
