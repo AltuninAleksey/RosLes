99ed3214470b365026f0ddfc4760ad6e1173bc34
AltuninAleksey <altunin.lescha@gmail.com>
2023-04-30 14:11:47 +0300

    dashboard_fix

--- app/src/main/java/com/example/rosles/Screens/Dashboard.kt
+++ app/src/main/java/com/example/rosles/Screens/Dashboard.kt
@@ -85,7 +85,7 @@ class Dashboard: AppCompatActivity() {
         }
 
         binding.ALLDOWNLOAD.setOnClickListener{
-            synhro.main1(viewModel,db,this)
+            synhro. main1(viewModel,db,this)
             viewModel.uploadbd.observe(this){
                 Log.v(it.msg.toString(),"")
             }
