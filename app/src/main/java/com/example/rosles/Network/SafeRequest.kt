d9f9abd5b2ceafcb5be2cdefff803a6eaca94af0
vladissslav <vladissslav19@gmail.com>
2023-04-30 21:30:59 +0300

    stash save

---
+++ app/src/main/java/com/example/rosles/Network/SafeRequest.kt
@@ -0,0 +1,46 @@
+package com.example.rosles.Network
+
+import androidx.lifecycle.viewModelScope
+
+import com.example.rosles.ResponceClass.BaseResponceInterface
+import kotlinx.coroutines.launch
+
+
+class SafeRequest(val viewModel: ViewModels) {
+
+    interface Protection {
+        suspend fun makeRequest() : BaseResponceInterface?
+
+        fun ifSuccess(responce: BaseResponceInterface?)
+
+        fun ifException() {}
+        fun ifConnectionException() {}
+        fun ifBackendException() {}
+        fun ifAuthException() {}
+    }
+
+    var responce: BaseResponceInterface? = null
+    val logger: Logger = LogCatLogger
+
+    fun request (protection: Protection) {
+        viewModel.viewModelScope.launch {
+            try {
+                responce = protection.makeRequest()
+                protection.ifSuccess(responce)
+            } catch (e: ConnectionException) {
+                logger.error("", e)
+                protection.ifConnectionException()
+            } catch (e: BackendException) {
+                logger.error("", e)
+                protection.ifBackendException()
+            } catch (e: AuthException) {
+                logger.error("", e)
+                protection.ifAuthException()
+            } catch (e: Exception) {
+                logger.error("", e)
+                protection.ifException()
+            }
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/rosles/ResponceClass/BaseResponceInterface.kt
@@ -0,0 +1,4 @@
+package com.example.rosles.ResponceClass
+
+interface BaseResponceInterface {
+}
