65fb13a8e460ca2ccb35082bf00ac5b0cef02050
vladissslav <vladissslav19@gmail.com>
2023-06-20 04:25:17 +0300

    update: improved class MapTrack. Added second type of lines on map

--- app/src/main/java/com/example/rosles/Screens/MapTrack.kt
+++ app/src/main/java/com/example/rosles/Screens/MapTrack.kt
@@ -1,5 +1,7 @@
 package com.example.rosles.Screens
 
+import android.graphics.Color
+import android.location.Location
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.viewModels
@@ -7,11 +9,12 @@ import com.example.rosles.BaseActivity
 import com.example.rosles.Network.ViewModels
 import com.example.rosles.databinding.MapLayoutBinding
 import com.example.rosles.utils.gps.simpleframework.ParseGps
+import com.example.rosles.utils.mapkit.TokenMapKitYandex.TOKEN
 import com.yandex.mapkit.Animation
 import com.yandex.mapkit.MapKitFactory
 import com.yandex.mapkit.geometry.Point
+import com.yandex.mapkit.geometry.Polyline
 import com.yandex.mapkit.map.CameraPosition
-import java.lang.StringBuilder
 
 
 class MapTrack: BaseActivity("Трек на карте") {
@@ -27,7 +30,6 @@ class MapTrack: BaseActivity("Трек на карте") {
 
         listOfTrack = intent.getSerializableExtra("listOfTrack")
                 as ArrayList<ParseGps.Waypoint>
-
         initMap()
     }
 
@@ -44,7 +46,7 @@ class MapTrack: BaseActivity("Трек на карте") {
     }
 
     private fun initMapKitFactory() {
-        MapKitFactory.setApiKey("67672189-f64b-4cfc-b019-3f14d2e65e70")
+        MapKitFactory.setApiKey(TOKEN)
         MapKitFactory.initialize(this)
     }
 
@@ -58,16 +60,65 @@ class MapTrack: BaseActivity("Трек на карте") {
 
     private fun initMap() {
         if (!listOfTrack.isEmpty()) {
-            binding.yandexMap.map.move(
-                CameraPosition(Point(listOfTrack[0].lat, listOfTrack[0].lon),
-                20.0f, 0.0f, 0.0f),
-                Animation(Animation.Type.SMOOTH, 2f), null
-            )
+            toZoom(listOfTrack.first().lat, listOfTrack.first().lon)
 
-            val placeMark = binding.yandexMap.map.mapObjects.addCollection().addPlacemark(
-                Point(listOfTrack[0].lat, listOfTrack[0].lon)
-            )
-            placeMark.opacity = 1.5f
+            printTrack()
         }
+    }
+
+    private fun toZoom(lat: Double, lon: Double) {
+        binding.yandexMap.map.move(
+            CameraPosition(Point(lat, lon),
+                20.0f, 0.0f, 0.0f),
+            Animation(Animation.Type.SMOOTH, 2f), null
+        )
+    }
+
+    private fun printTrack() {
+        for (i in 0 until  listOfTrack.size - 1 ) {
+            val p1 = Point(listOfTrack[i].lat, listOfTrack[i].lon)
+            val p2 = Point(listOfTrack[i+1].lat, listOfTrack[i+1].lon)
+            val distanceBetweenPoints = getDistanceBetweenPoints(p1, p2)
+
+            printPoint(p1)
+            printPoint(p2)
+
+            if (distanceBetweenPoints >= 100f) {
+                printRedLine(p1, p2)
+            } else {
+                printLine(p1, p2)
+            }
+
+        }
+    }
+
+    private fun printPoint(p: Point) {
+        binding.yandexMap.map.mapObjects.addCollection().addPlacemark(
+            Point(p.latitude, p.longitude)
+        ).opacity = 1.5f
+    }
+
+    private fun printLine(p1: Point, p2: Point) {
+        binding.yandexMap.map.mapObjects.addCollection().addPolyline(
+            Polyline(listOf(p1, p2))
+        )
+    }
+
+    private fun printRedLine(p1: Point, p2: Point) {
+        binding.yandexMap.map.mapObjects.addCollection().addPolyline(
+            Polyline(listOf(p1, p2))
+        ).setStrokeColor(Color.RED)
+    }
+
+    private fun getDistanceBetweenPoints(p1: Point, p2: Point,): Float {
+        val loc1 = Location("")
+        loc1.latitude = p1.latitude
+        loc1.longitude = p1.longitude
+
+        val loc2 = Location("")
+        loc2.latitude = p2.latitude
+        loc2.longitude = p1.longitude
+
+        return loc1.distanceTo(loc2)
     }
 }
---
+++ app/src/main/java/com/example/rosles/utils/mapkit/TokenMapKitYandex.kt
@@ -0,0 +1,5 @@
+package com.example.rosles.utils.mapkit
+
+object TokenMapKitYandex {
+    const val TOKEN: String = "67672189-f64b-4cfc-b019-3f14d2e65e70"
+}
