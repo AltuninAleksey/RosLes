0b0ceae8ea5b50b2730d8c0e240f991a2fb40ede
Ivanov Pavel <87614770+cyberkitaec@users.noreply.github.com>
2023-07-24 20:52:54 +0300

    fix formdocx now abs path

--- testDjangosite/staticpy/forming_docx.py
+++ testDjangosite/staticpy/forming_docx.py
@@ -1,22 +1,21 @@
 from testDjangosite.settings import BASE_DIR
-import os
 from docxtpl import DocxTemplate
+import os
 import uuid
 # название включает себя fieldcard_{id}
 # название описание участка тоже самое
 # две новых таблицы для хранения ссылок на полевую карточку и описание участка
 
 def forming_docx_fieldcard(context: dict):
-    doc = DocxTemplate(f"{BASE_DIR}\\media\\fieldcard.docx")
+    doc = DocxTemplate(os.path.abspath(f"{BASE_DIR}\\media\\fieldcard.docx"))
     doc.render(context)
-    filepath = f"{BASE_DIR}\\media\\docx_files\\fieldcards\\fieldcard_{context['id']}.docx"
+    filepath = os.path.abspath(f"{BASE_DIR}\\media\\docx_files\\fieldcards\\fieldcard_{context['id']}.docx")
     doc.save(filepath)
     return filepath.split("testDjangosite")[1]
 
 
 def forming_docx_desc_region(context: dict):
-    doc = DocxTemplate(f"{BASE_DIR}\\media\\desc_region.docx")
+    doc = DocxTemplate(os.path.abspath(f"{BASE_DIR}\\media\\desc_region.docx"))
     doc.render(context)
-    filepath = f"{BASE_DIR}\\media\\docx_files\\desc_region\\desc_region_{context['id']}.docx"
-    doc.save(filepath)
+    filepath = os.path.abspath(f"{BASE_DIR}\\media\\docx_files\\desc_region\\desc_region_{context['id']}.docx")
     return filepath.split("testDjangosite")[1]
