b84dd23d002cd7e9f8f6ccaac726f8402cf90ad4
Ivanov Pavel <87614770+cyberkitaec@users.noreply.github.com>
2023-07-24 16:00:59 +0300

    new url desc region form docx

--- testDjangosite/testDjangosite/urls.py
+++ testDjangosite/testDjangosite/urls.py
@@ -115,10 +115,13 @@ urlpatterns = [
     path('plotcoeff/<int:pk>', PlotCoeffViews.as_view()),
     path('plotcoeff/fieldcard/<int:id_field_card>', PlotCoeffByFieldCardId.as_view()),
     path('formdocx', FormingDocxView.as_view()),
+    path('formdocxdesc', FormingDocxViewDescRegion.as_view()),
     path('calculateratio', RatioCompositionCalculateInList.as_view()),
     path('erp/', include('erp.urls')),
     path('general_docs/', include('general_docs.urls')),
+
 ]
+
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
