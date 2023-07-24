5b16940ca70561c8d459d9b438819b789214a9cb
Ivanov Pavel <87614770+cyberkitaec@users.noreply.github.com>
2023-07-24 21:28:33 +0300

    update path forming docx

--- testDjangosite/staticpy/forming_docx.py
+++ testDjangosite/staticpy/forming_docx.py
@@ -7,15 +7,15 @@ import uuid
 # две новых таблицы для хранения ссылок на полевую карточку и описание участка
 
 def forming_docx_fieldcard(context: dict):
-    doc = DocxTemplate(os.path.abspath(f"{BASE_DIR}\\media\\fieldcard.docx"))
+    doc = DocxTemplate(os.path.abspath(f"{BASE_DIR}/media/fieldcard.docx"))
     doc.render(context)
-    filepath = os.path.abspath(f"{BASE_DIR}\\media\\docx_files\\fieldcards\\fieldcard_{context['id']}.docx")
+    filepath = os.path.abspath(f"{BASE_DIR}/media/docx_files/fieldcards/fieldcard_{context['id']}.docx")
     doc.save(filepath)
     return filepath.split("testDjangosite")[1]
 
 
 def forming_docx_desc_region(context: dict):
-    doc = DocxTemplate(os.path.abspath(f"{BASE_DIR}\\media\\desc_region.docx"))
+    doc = DocxTemplate(os.path.abspath(f"{BASE_DIR}/media/desc_region.docx"))
     doc.render(context)
-    filepath = os.path.abspath(f"{BASE_DIR}\\media\\docx_files\\desc_region\\desc_region_{context['id']}.docx")
+    filepath = os.path.abspath(f"{BASE_DIR}/media/docx_files/desc_region/desc_region_{context['id']}.docx")
     return filepath.split("testDjangosite")[1]
