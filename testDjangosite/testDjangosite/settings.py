b60c60e2383e5313f88a650708050ce9053d4f69
Ivanov Pavel <87614770+cyberkitaec@users.noreply.github.com>
2023-08-09 15:20:44 +0300

    settings to database postgres

--- testDjangosite/testDjangosite/settings.py
+++ testDjangosite/testDjangosite/settings.py
@@ -86,13 +86,13 @@ WSGI_APPLICATION = 'testDjangosite.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+#
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
@@ -116,13 +116,24 @@ REST_FRAMEWORK = {
 
 
 # Postgres
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'rosLes',
+        'USER': 'postgres',
+        'PASSWORD': 'PPythonUserassword',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
+#
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'rosLes',
-#         'USER': 'postgres',
-#         'PASSWORD': 'PPythonUserassword',
-#         'HOST': '127.0.0.1',
+#         'NAME': 'rosles',
+#         'USER': 'roslesuser',
+#         'PASSWORD': 'roslesuser',
+#         'HOST': '90.156.208.88',
 #         'PORT': '5432',
 #     }
 # }
