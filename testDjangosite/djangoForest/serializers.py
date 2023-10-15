f42d6d8fca47bd07168aa9ac736015246e74a40f
cyberkitaec <addkudi@mail.ru>
2023-10-15 21:32:42 +0300

    add instance lands_other in update method FieldCardSerializer

--- testDjangosite/djangoForest/serializers.py
+++ testDjangosite/djangoForest/serializers.py
@@ -746,6 +746,7 @@ class FieldCardSerializer(serializers.ModelSerializer):
         instance.point7_completeness = validated_data.get("point7_completeness")
         instance.point7_stock = validated_data.get("point7_stock")
         instance.number_order = validated_data.get("number_order")
+        instance.lands_other = validated_data.get('lands_other')
         instance.save()
         return instance
 
