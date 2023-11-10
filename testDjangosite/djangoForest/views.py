427a8e4b49783900fa03605bb6daf52d324c0f0c
cyberkitaec <addkudi@mail.ru>
2023-11-10 19:14:27 +0300

    log

--- testDjangosite/djangoForest/views.py
+++ testDjangosite/djangoForest/views.py
@@ -1301,6 +1301,7 @@ class GetFieldCard(ListAPIView):
                              "error_text": desc_serializer.errors[next(iter(desc_serializer.errors))][0]},
                             status=status.HTTP_400_BAD_REQUEST)
         if not fieldcard_serializer.is_valid():
+            print(fieldcard_serializer.errors)
             return Response({"error_fieldcard": status.HTTP_400_BAD_REQUEST,
                              "error_text": fieldcard_serializer.errors[next(iter(fieldcard_serializer.errors))][0]},
                             status=status.HTTP_400_BAD_REQUEST)
