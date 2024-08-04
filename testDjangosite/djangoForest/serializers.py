402a01b2dffb16d5c1ec5d4d6cfa322bc7e3d19c
Ivanov Pavel <87614770+cyberkitaec@users.noreply.github.com>
2024-08-04 17:19:07 +0300

    fix error with save details_regulations

--- testDjangosite/djangoForest/serializers.py
+++ testDjangosite/djangoForest/serializers.py
@@ -850,6 +850,7 @@ class FieldCardSerializer(serializers.ModelSerializer):
         instance.respond_farm = validated_data.get('respond_farm')
         instance.start_at = validated_data.get('start_at')
         instance.end_at = validated_data.get('end_at')
+        instance.details_regulations = validated_data.get('details_regulations')
         instance.save()
         return instance
 
