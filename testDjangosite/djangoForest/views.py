f84b30cc154007fb8c0292c954868b8c3341f97c
Ivanov Pavel <87614770+cyberkitaec@users.noreply.github.com>
2025-06-09 21:21:35 +0300

    fix listregionfilters

--- testDjangosite/djangoForest/views.py
+++ testDjangosite/djangoForest/views.py
@@ -1368,7 +1368,7 @@ class ListRegionFilters(ListAPIView):
             id_subject = request.user.id_subject_rf
         except:
             print("Cannot find user")
-        ser2 = ListRegion.objects.filter(id_district_forestly__id_forestly__id_subject_rf == id_subject)
+        ser2 = ListRegion.objects.filter(id_district_forestly__id_forestly__id_subject_rf = id_subject)
         if request.data['bSubjectrf']:
             ser2 = ListRegion.objects.all()
             idSubjectrf = request.data['idSubjectrf']
