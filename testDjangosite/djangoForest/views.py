bf78ac4e64d84129dfed44d11897a783863a8b67
Ivanov Pavel <87614770+cyberkitaec@users.noreply.github.com>
2025-06-09 21:03:46 +0300

    id_subject_rf filters

--- testDjangosite/djangoForest/views.py
+++ testDjangosite/djangoForest/views.py
@@ -1365,7 +1365,7 @@ class ListRegionFilters(ListAPIView):
 
     def post(self, request, *args, **kwargs):
         try:
-            id_subject = request.user.subject_rf_id
+            id_subject = request.user.id_subject_rf
         except:
             print("Cannot find user")
         ser2 = ListRegion.objects.filter(id_district_forestly__id_forestly__id_subject_rf == id_subject)
