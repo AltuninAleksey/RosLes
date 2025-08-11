e9f4381ca530ef5676c16dfd4f75012209783f33
Ivanov Pavel <87614770+cyberkitaec@users.noreply.github.com>
2025-08-11 21:08:22 +0300

    add new if in save listreigon

--- testDjangosite/djangoForest/models.py
+++ testDjangosite/djangoForest/models.py
@@ -211,7 +211,7 @@ class ListRegion(models.Model):
 
     def save(self, *args, **kwargs):
         super(ListRegion, self).save(*args, **kwargs)
-        if self.number_region is None:
+        if self.number_region is None or self.number_region == 0 or self.number_region == '0':
             id_czl = get_local_id_czl(self.id_profile.id)
 
             ListRegion.objects.filter(id=self.id).update(number_region = id_czl['number_region__max'] + 1)
