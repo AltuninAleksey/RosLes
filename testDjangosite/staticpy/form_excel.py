6c87f8da06c01471fd0221ecec379ef43712b15f
Ivanov Pavel <87614770+cyberkitaec@users.noreply.github.com>
2025-12-20 16:33:10 +0300

    Исправил ошибку в формуле. Теперь делится на площадь 1 га

--- testDjangosite/staticpy/form_excel.py
+++ testDjangosite/staticpy/form_excel.py
@@ -606,7 +606,7 @@ def list_region_excel(data: dict, podlesok: bool = True, others: bool = False):
         try:
             cell =  sheets.cell(row=cur_row + 1, column=i)
             res = (data['square'] / len_samples_res) / 10000
-            sheets.cell(row=cur_row + 2, column=i, value=round(cell.value*10000)/400)
+            sheets.cell(row=cur_row + 2, column=i, value=round(cell.value*10000)/data['square'])
         except:
             pass
         sheets.merge_cells(start_row=cur_row+2, start_column=i, end_row=cur_row+2, end_column=i+4)
