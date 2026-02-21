e3b873588f3ec35ff8a7c593312c9d0d4bf94458
Ivanov Pavel <87614770+cyberkitaec@users.noreply.github.com>
2026-02-21 22:23:07 +0300

    urls для скачивания архива

--- testDjangosite/testDjangosite/urls.py
+++ testDjangosite/testDjangosite/urls.py
@@ -30,6 +30,7 @@ urlpatterns = [
     re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     # path('exel', testviews.as_view()),
     # path('form_undergrowth', CreateUndergrowthExcel.as_view()),
+    path('zip_photos/<int:id>', DownloadPhotosArchiveView.as_view()),
     path('change_password', ChangePasswordView.as_view()),
     path('change_password_with_old', ChangePasswordWithOldView.as_view()),
     path('forest_districts', ForestDistrictView.as_view()),
