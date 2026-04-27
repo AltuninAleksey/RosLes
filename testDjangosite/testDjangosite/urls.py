8c86fd3186cc24a7eba772d36cfb726a024281a2
Ivanov Pavel <87614770+cyberkitaec@users.noreply.github.com>
2026-04-28 00:37:19 +0300

    new url

--- testDjangosite/testDjangosite/urls.py
+++ testDjangosite/testDjangosite/urls.py
@@ -32,6 +32,7 @@ urlpatterns = [
     # path('form_undergrowth', CreateUndergrowthExcel.as_view()),
     path('zip_photos/<int:id>', DownloadPhotosArchiveView.as_view()),
     path('czl_id', CzlIdByProfile.as_view()),
+    path('get_czl_subject_data', GetCzlInfoByProfile.as_view()),
     path('change_password', ChangePasswordView.as_view()),
     path('change_password_with_old', ChangePasswordWithOldView.as_view()),
     path('forest_districts', ForestDistrictView.as_view()),
