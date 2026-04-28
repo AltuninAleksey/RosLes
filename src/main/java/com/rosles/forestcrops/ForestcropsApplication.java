2abb85476a5a748d5e69a243c5144ba331138364
Shock <polenok.maksim.2001@mail.ru>
2026-04-28 11:41:42 +0300

    mpo: init

---
+++ src/main/java/com/rosles/forestcrops/ForestcropsApplication.java
@@ -0,0 +1,13 @@
+package com.rosles.forestcrops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForestcropsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForestcropsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rosles/forestcrops/ForestcropsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rosles.forestcrops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForestcropsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
