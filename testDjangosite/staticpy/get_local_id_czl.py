ed94c5f9e310a8d0fb73a423a74c6a3e52fbee33
Ivanov Pavel <87614770+cyberkitaec@users.noreply.github.com>
2026-04-29 11:19:20 +0300

    Изменил поведеения фильтрации

--- testDjangosite/staticpy/get_local_id_czl.py
+++ testDjangosite/staticpy/get_local_id_czl.py
@@ -7,6 +7,7 @@ from django.db.models import Q
 def get_local_id_czl(id_profile: int) -> int:
     from djangoForest.models import Profile, CZL, ListRegion, SubjectRF
     lst_czl = []
+
     id_subject = Profile.objects.filter(id = id_profile).values('id_user_id__subject_rf_id')[0]
     if id_subject['id_user_id__subject_rf_id'] == 27:
         lst_region = ListRegion.objects.filter(id_district_forestly__id_forestly__id_subject_rf=27)
@@ -21,7 +22,8 @@ def get_local_id_czl(id_profile: int) -> int:
             list_czl = CZL.objects.filter(id_main_subject = main_czl['id_main_subject']).values('id_subject_id')
             # list_czl.append(main_czl[0])
         lst_subject = SubjectRF.objects.filter(Q(id__in = list_czl) | Q(id = main_czl['id_main_subject']))
-        lst_region = ListRegion.objects.filter(id_profile_id__id_user_id__subject_rf_id__in = lst_subject)
+        lst_region = ListRegion.objects.filter(id_district_forestly__id_forestly__id_subject_rf__in = lst_subject)
+    print(len(lst_region))
     id_czl = lst_region.aggregate(Max('number_region'))
     if id_czl['number_region__max'] is None:
         id_czl['number_region__max'] = 0
